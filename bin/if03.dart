d5d526afd0d457ef5c111873a9387bac20372a89
Sultonbek <idrisovsultonbek@gmail.com>
2023-04-28 14:06:33 +0500

    03

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,34 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
-void main() {
+   int func(int a, b, c) {
+    
+  if(a > b) {
+    if( b > c) {
+      print(b);
+    }
+    if(a > c) {
+      return c;
+    }else {
+      return a;
+    }
+  }
+  else {
+    if(a > c){
+      return a;
+    }
+    if(b > c) {
+    return c;
+    }
+    else {
+      return b;
+    }
+  }
+
 }
+void main() {
+  print(func(77, 88, 99));
+}
+
+
+
