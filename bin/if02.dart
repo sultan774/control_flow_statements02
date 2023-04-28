608cc4aa407a7473832c3b1e532177b4cf585c66
Sultonbek <idrisovsultonbek@gmail.com>
2023-04-28 14:06:14 +0500

    02

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,22 @@ Args:
 Returns:
     int: return answer.
 */
-void main() {
+int func(int a, int b, int c) {
+  if(a < b) {
+    if(b < c) {
+      return a;
+    } else {
+      return c;
+    }
+  } else {
+    if(b < c) {
+      return b;
+    } else {
+      return c;
+    }
+  }
 }
+void main() {
+  print(func(7, 77, 777));
+}
+
